1d8786a545926091821e9827584a0e54f00e9ee6
bhattrahul86 <rahul.itsmymail@gmail.com>
2018-12-25 21:46:47 +0530

    react redux basic setup

---
+++ redux-intro/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Counter from './Counter';
+
+const initialState = {
+    count: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const App = () => (
+  <div>
+  <Provider store = {store}>
+    <Counter />
+  </Provider>  
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
